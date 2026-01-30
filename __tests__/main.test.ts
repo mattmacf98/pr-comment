bf6f4a2d98c41c654b9b98b8f61849d9989c3833
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-01-30 11:35:55 -0500

    add logic

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,57 +6,17 @@
  * so that the actual '@actions/core' module is not imported.
  */
 import { jest } from '@jest/globals'
-import * as core from '../__fixtures__/core.js'
-import { wait } from '../__fixtures__/wait.js'
+import * as core from '../__fixtures__/core.ts'
 
 // Mocks should be declared before the module being tested is imported.
 jest.unstable_mockModule('@actions/core', () => core)
-jest.unstable_mockModule('../src/wait.js', () => ({ wait }))
 
 // The module being tested should be imported dynamically. This ensures that the
 // mocks are used in place of any actual dependencies.
-const { run } = await import('../src/main.js')
+const { run } = await import('../src/main.ts')
 
 describe('main.ts', () => {
-  beforeEach(() => {
-    // Set the action's inputs as return values from core.getInput().
-    core.getInput.mockImplementation(() => '500')
-
-    // Mock the wait function so that it does not actually wait.
-    wait.mockImplementation(() => Promise.resolve('done!'))
-  })
-
-  afterEach(() => {
-    jest.resetAllMocks()
-  })
-
-  it('Sets the time output', async () => {
+  it('should create a comment on the PR', async () => {
     await run()
-
-    // Verify the time output was set.
-    expect(core.setOutput).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      // Simple regex to match a time string in the format HH:MM:SS.
-      expect.stringMatching(/^\d{2}:\d{2}:\d{2}/)
-    )
-  })
-
-  it('Sets a failed status', async () => {
-    // Clear the getInput mock and return an invalid value.
-    core.getInput.mockClear().mockReturnValueOnce('this is not a number')
-
-    // Clear the wait mock and return a rejected promise.
-    wait
-      .mockClear()
-      .mockRejectedValueOnce(new Error('milliseconds is not a number'))
-
-    await run()
-
-    // Verify that the action was marked as failed.
-    expect(core.setFailed).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds is not a number'
-    )
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import { getOctokit } from '@actions/github'
 
 /**
  * The main function for the action.
@@ -8,18 +8,61 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const owner = core.getInput('owner', { required: true })
+    const repo = core.getInput('repo', { required: true })
+    const prNumber = core.getInput('pr_number', { required: true })
+    const token = core.getInput('token', { required: true })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const octokit = getOctokit(token)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: parseInt(prNumber, 10)
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    let diffData = {
+      additions: 0,
+      deletions: 0,
+      changes: 0
+    }
+
+    diffData = changedFiles.reduce((acc, file) => {
+      acc.additions += file.additions
+      acc.deletions += file.deletions
+      acc.changes += file.changes
+      return acc
+    }, diffData)
+
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: parseInt(prNumber, 10),
+      body: `This PR contains ${diffData.additions} additions, ${diffData.deletions} deletions, and ${diffData.changes} changes.`
+    })
+
+    const labels = new Set<string>()
+    for (const file of changedFiles) {
+      const fileExtension = file.filename.split('.').pop()
+      switch (fileExtension) {
+        case 'js':
+          labels.add('javascript')
+          break
+        case 'ts':
+          labels.add('typescript')
+          break
+        default:
+          labels.add('other')
+          break
+      }
+    }
+
+    await octokit.rest.issues.addLabels({
+      owner,
+      repo,
+      issue_number: parseInt(prNumber, 10),
+      labels: Array.from(labels)
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
